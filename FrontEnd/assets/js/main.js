45feed9579df5ec48059f99e6e9f024b634a0f8a
Muhamet1 <mh51221@ubt-uni.net>
2022-11-12 00:17:00 +0100

    Front End Design

--- FrontEnd/assets/js/main.js
+++ FrontEnd/assets/js/main.js
@@ -70,7 +70,6 @@ fetch("https://localhost:7114/api/Projects")
 var form = document.getElementById("form");
 
 form.addEventListener("submit", function (e) {
-  //auto submit
   
   var name = document.getElementById("name").value;
   var email = document.getElementById("email").value;
@@ -93,11 +92,29 @@ form.addEventListener("submit", function (e) {
     })
     .then(function (data) {
       console.log(data);
-    });
+    }).then(alert("Message sent succesfully"))
 });
 
 
 
+/*SCROLLL DELAY ********/
 
+function reveal() {
+  var reveals = document.querySelectorAll(".reveal");
+
+  for (var i = 0; i < reveals.length; i++) {
+    var windowHeight = window.innerHeight;
+    var elementTop = reveals[i].getBoundingClientRect().top;
+    var elementVisible = 150;
+
+    if (elementTop < windowHeight - elementVisible) {
+      reveals[i].classList.add("active");
+    } else {
+      reveals[i].classList.remove("active");
+    }
+  }
+}
+
+window.addEventListener("scroll", reveal);
  
   
